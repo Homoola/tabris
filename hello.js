70b6d58c38716dc93e5468889602c1cdd1da2f68
Homoola <tabris.homoola@gmail.com>
2016-07-02 17:01:18 +0000

    add vibrat

--- hello.js
+++ hello.js
@@ -1,3 +1,6 @@
+
+
+
 var page = tabris.create("Page", {
   title: "homoola calander",
   topLevel: true
@@ -11,15 +14,24 @@ tabris.create("ESCalendar", {
 }).on("change:date", function(widget, date) {
   console.error(new Date().toUTCString());
 }).appendTo(page);
-
+//
 tabris.create("Button", {
   id: "button",
   text: "Get Date",
-  layoutData: {left: 16, right: 16, bottom: 16}
+  layoutData: {left: 16, right: 16}
 }).on("select", function() {
   var date = page.children("#calendar").get("date");
   console.log("date: " + new Date(parseInt(date)).toUTCString());
 }).appendTo(page);
+//
+tabris.create("Button", {
+  id: "button",
+  text: "contact",
+  layoutData: {left: 16, right: 16}
+}).on("select", function() {
+  navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
+}).appendTo(page);
+
 
 
 
